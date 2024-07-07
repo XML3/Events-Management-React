436822429e1d6d351527bdcb8020e214e0456815
Xagly Montilva <xaglymontilva@gmail.com>
2024-07-07 17:27:43 +0200

    deleted

--- starter/src/components/UI/constants.js
+++ starter/src/components/UI/constants.js
@@ -1,16 +1,3 @@
-const API_URL = "https://bed-express-event-api.onrender.com";
-// const API_URL = " http://localhost:3000";
-
-export const API_ROUTES = {
-  SIGN_UP: `${API_URL}/auth/signup`,
-  SIGN_IN: `${API_URL}/auth/signin`,
-  GET_USER: `${API_URL}/auth/me`,
-};
-
-export const APP_ROUTES = {
-  SIGN_UP: "/auth/signup",
-  SIGN_IN: "/auth/signin",
-  DASHBOARD: "/dashboard",
-};
+const API_URL = " http://localhost:3000";
 
 export { API_URL };
