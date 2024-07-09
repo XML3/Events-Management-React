ea8f9008ab0d47732fdda7d3c1c14db3ebc8e089
Xagly Montilva <xaglymontilva@gmail.com>
2024-07-09 13:24:58 +0200

    back

--- starter/src/pages/EventsPage.jsx
+++ starter/src/pages/EventsPage.jsx
@@ -238,7 +238,7 @@ export const EventsPage = () => {
                       <EventsCard
                         key={event.id}
                         event={event}
-                        categories={categories.id}
+                        categories={categories}
                       />
                     </Link>
                   ))}
