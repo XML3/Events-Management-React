d2459ad41b356fd388c8e63ccd3d7c67784a687c
Xagly Montilva <xaglymontilva@gmail.com>
2024-07-10 16:48:28 +0200

    testing createdBy

--- starter/src/pages/EventPage.jsx
+++ starter/src/pages/EventPage.jsx
@@ -408,7 +408,21 @@ export const EventPage = () => {
                   fontSize={{ base: "0.9rem", md: "0.8rem" }}
                   letterSpacing={{ base: "0.05rem", md: "0.06rem" }}
                 >
-                  <UserPage userId={creator.id} />
+                  {/* <UserPage userId={creator.id} /> */}
+                  <Box>
+                    {event.createdBy.map((creatorId) => {
+                      const creator = users.find(
+                        (creator) => creator.id === creatorId
+                      );
+                      if (!creator) return null;
+
+                      return (
+                        <Text key={creator.id}>
+                          {creator.name && creator.image}
+                        </Text>
+                      );
+                    })}
+                  </Box>
                 </Box>
               </Grid>
 
