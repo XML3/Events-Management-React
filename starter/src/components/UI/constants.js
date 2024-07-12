072293ff70eda2acbf7410b27506081fe40d9087
Xagly Montilva <xaglymontilva@gmail.com>
2024-07-12 18:42:43 +0200

    modifed to fix eventData.createdBy as an Object conflict, modified to take image for event and user as urls, fetched directly from Root and removed UserPage's fetch

--- starter/src/components/UI/constants.js
+++ starter/src/components/UI/constants.js
@@ -1,5 +1,11 @@
-const API_URL = "https://bed-express-event-api.onrender.com";
+//hosted URL
+// const API_URL = "https://bed-express-event-api.onrender.com";
+
+//local url
 // const API_URL = " http://localhost:3000";
+
+//Talking to the Back-end locally URL
+const API_URL = " http://localhost:4000";
 
 export const API_ROUTES = {
   SIGN_UP: `${API_URL}/auth/signup`,
