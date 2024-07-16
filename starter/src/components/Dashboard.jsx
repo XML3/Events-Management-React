d2084acc17c58092a9058f7ac775f2a79d230b2c
Xagly Montilva <xaglymontilva@gmail.com>
2024-07-16 11:36:14 +0200

    modified style

--- starter/src/components/Dashboard.jsx
+++ starter/src/components/Dashboard.jsx
@@ -7,8 +7,17 @@ const Dashboard = () => {
   if (!user || !authenticated) {
     return (
       <div className="p-16 bg-gray-800 h-screen">
-        <div className="text-2xl mb-4 font-bold text-white">Dashboard</div>
-        <div className="ml-2 w-8 h-8 border-l-2 rounded-full animate-spin border-white" />
+        <Text fontSize={"24px"} mb={4}>
+          Dashboard
+        </Text>
+        <Box
+          ml={2}
+          w={8}
+          h={8}
+          borderLeft={2}
+          borderRadius={"full"}
+          borderColor={"white"}
+        />
       </div>
     );
   }
