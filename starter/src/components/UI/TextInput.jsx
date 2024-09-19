5c1521ba3f555d8185383aff290f1decbf712e21
Xagly Montilva <xaglymontilva@gmail.com>
2024-09-19 19:48:56 +0200

    modified border colour

--- starter/src/components/UI/TextInput.jsx
+++ starter/src/components/UI/TextInput.jsx
@@ -6,7 +6,7 @@ export const TextInput = ({ changeFn, ...props }) => {
       <Input
         ml={3}
         variant="outline"
-        borderColor={"#051622"}
+        borderColor={"#d5d1bf"}
         focusBorderColor="#610979"
         w={"23rem"}
         mt={"1rem"}
